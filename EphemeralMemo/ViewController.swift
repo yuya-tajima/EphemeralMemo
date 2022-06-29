f6245e6fe683b3e68d83b374913f11b90f787ac2
tajima_taso <tajimataso@gmail.com>
2022-06-30 07:10:41 +0900

    Initial Commit

---
+++ EphemeralMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EphemeralMemo
+//
+//  Created by 優也田島 on 2022/06/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
